4d3f195e4618e0b3c8509e0b46e2faa2257e2645
ammarhus1 <ammarhus@live.com>
2017-10-08 01:09:59 -0400

    Added methods so items can be places in rooms

--- classes/Item.php
+++ classes/Item.php
@@ -9,16 +9,25 @@ class Item{
     public $type;
     public $stackable;
     public $amount;
+
     // Jarrod is awesome
-    public function __construct($id, $name, $description, $sellValue, $type, $stackable, $buyValue) {
+    public function __construct($id, $name, $description, $type, $stackable, $buyValue, $sellValue) {
         $this->id=$id;
         $this->name=$name;
         $this->description=$description;
-        $this->sellValue=$sellValue;
         $this->type=$type;
         $this->stackable=$stackable;
-        $this->amount = 0;
-		    $this->buyValue = $buyValue;
+        $this->amount=0;
+		$this->buyValue=$buyValue;
+        $this->sellValue=$sellValue;
+    }
+    public $dropid;
+    public static function itemDrop( $id , $amount , $dropid ) {
+        $instance = new self($id, "", "", "", 0, "","");
+        $instance->id = $id;
+        $instance->amount = $amount;
+        $instance->dropid = $dropid;
+        return $instance;
     }
 /*
     public function __clone() {
