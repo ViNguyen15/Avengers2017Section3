6657287805d92334650591c7f4a5baa62706dd17
ammarhus1 <ammarhus@live.com>
2017-10-10 01:11:25 -0400

    HTML more organized

--- register.php
+++ register.php
@@ -5,54 +5,58 @@
 <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
 <link rel="stylesheet" type="text/css" href="styles.css?v=<?php echo time();?>" />
 </head>
-<body>
-<div id="container">
-<div id="header"><h1>Text-Based Game</h1></div>
-  <div id="wrapper">
-    <div id="content">      
 
-        <form action="function/create_user.php" method="post">
+<body>
+<header>Header</header>
+<div id="main">
+
+<article>
+    <h2>New User Login</h2>
+    <form action="function/create_user.php" method="post">
         Username: <input type="text" name="username"><br>
         Password: <input type="password" name="password"><br>
         Re-type Password: <input type="password" name="password2"><br>
         <input type="submit" value="register">
-        </form>
+    </form>
 
-        <?php 
-        
-        $i = 0;
+</article>
 
-        if (isset($_GET["error"])){
-            $i = $_GET["error"];
-        }
-        switch ($i) {
-            case 1:
-                echo "<font color=red>Username is taken!</font>";
-                break;
-            case 2:
-                echo "<font color=red>Passwords did not match.</font>";
-                break;
-            case 3:
-                echo "i equals 2";
-                break;
-        }
-
-        ?>
-    </div>
-  </div>
-  <div id="navigation">
-
-  </div>
-  <div id="extra">
+<nav>
+    <h2> Existing user login: </h2>
     <form action="function/login.php" method="post">
         Username: <input type="text" name="username"><br>
         Password: <input type="password" name="password"><br>
         <input type="submit" value="Login">
     </form>
-  </div>
-  <div id="footer">
-    <p>Footer</p>
-  </div>
+
+    <?php         
+    $i = 0;
+
+    if (isset($_GET["error"])){
+        $i = $_GET["error"];
+    }
+    switch ($i) {
+        case 1:
+            echo "<font color=red>Username is taken!</font>";
+            break;
+        case 2:
+            echo "<font color=red>Passwords did not match.</font>";
+            break;
+        case 3:
+            echo "i equals 2";
+            break;
+    }
+    ?>
+    
+</nav>
+
+<aside>Aside</aside>
+
 </div>
-</body>
+
+<footer>Footer</footer>
+
+</body> 
+
+
 </html>
