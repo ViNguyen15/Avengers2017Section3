b0f1e6c67b7cc86df3924be4b0bd9d5ea0252b62
ammarhus1 <ammarhus@live.com>
2017-10-11 18:19:03 -0400

    Display now sends to home base in case of error

--- function/display_room.php
+++ function/display_room.php
@@ -8,15 +8,8 @@ $game = $_SESSION['game'];
 if (isset($_SESSION['location'])){
     $_SESSION['location']->display();
 } else {
-
-    foreach($game as $index => $planet){
-        echo "
-            <form action='function/goto_planet.php'>
-            <button type='submit' name='system' value='$index'>Go to Planet $planet->name</button>
-            </form>
-            ";
-    }
-
+    $_SESSION['location'] = $_SESSION['game'][2]->rooms[3];
+    $_SESSION['location']->display();
 }
 
 ?>
