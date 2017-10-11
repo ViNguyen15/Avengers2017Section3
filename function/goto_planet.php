a322799b256be33c91abc7cffdc18d41eb7212ba
ammarhus1 <ammarhus@live.com>
2017-10-11 18:19:51 -0400

    Changed function to act as a button to go back to base

--- function/goto_planet.php
+++ function/goto_planet.php
@@ -5,6 +5,8 @@ foreach (glob("../classes/*.php") as $class) {
 }
 
 session_start();
+
+/*
 if(isset($_GET['room'])){
     $_SESSION['location'] = findPlanet($_SESSION['game'],$_GET['room']);
 }
@@ -12,7 +14,9 @@ if(isset($_GET['room'])){
 if(isset($_GET['system'])){
     $_SESSION['location'] = $_SESSION['game'][$_GET['system']];
 }
+*/
 
+$_SESSION['location'] = findRoom($_SESSION['game'],'12');
 
 
 
@@ -21,6 +25,15 @@ function findPlanet($planets,$id){
         foreach ($planet->rooms as $room){
             if ($room->id == $id){
                 return $planet;
+            }
+        }
+    }
+}
+function findRoom($planets,$id){
+    foreach ($planets as $planet){ 
+        foreach ($planet->rooms as $room){
+            if ($room->id == $id){
+                return $room;
             }
         }
     }
