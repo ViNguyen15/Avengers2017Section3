c17fa82f65819cdec04971dcdcab6ac4f5235e27
ammarhus1 <ammarhus@live.com>
2017-10-10 22:29:39 -0400

    Made the inventory pretty. Added descriptions

--- function/display_inv.php
+++ function/display_inv.php
@@ -1,17 +1,44 @@
+<inventory>
+Inventory:<br>
 <?php
+/**
+    Used in:    Index.php
+    Accesses:   classes/item.php
+*/
 
-$inventory = $_SESSION['inventory'];
 //Creates an array variable with all the inventory data from the session
+//include('/database/item.php');
+ $inventoryDisplay = $_SESSION['inventory'];
 
-
-echo "Inventory:<br>";
-
-foreach ($inventory as $item){
+foreach ($inventoryDisplay as $index => $item){
 //For each item in the inventory array
-    //if ($item->amount > 0){ 
-    //Remove the comments before if-statement to only show items the player has
-        echo "<div class='item'> <img src='./images/items/$item->id.png' height='32' width='32'> $item->name x $item->amount  </div>";
-    //}
-}
+    if ($item->amount > 0 || $item->id==0){  
+    //Remove any items that are at 0 value
+    echo "<item onmouseover='displayDescription(this)' onmouseout='removeDescription(this)'> 
+        <img src='./images/items/$item->id.png' height='32' width='32'>
+        <description><b><u>$item->name</u></b> <br> $item->description</description>
+        <amount>x$item->amount</amount>
+        </item>";
+    }
+    
+    }
 
 ?>
+<description id="desc">
+
+</description>
+<script>
+/*
+ * This script makes a description appear and disappear when you hover over the image of the item. 
+ */
+var description = document.getElementById("desc").innerHTML;
+function displayDescription(x) {
+    document.getElementById("desc").innerHTML = x.getElementsByTagName("description")[0].innerHTML; 
+}
+
+function removeDescription(x) {
+    document.getElementById("desc").innerHTML = " "; 
+}
+</script>
+
+</inventory>
