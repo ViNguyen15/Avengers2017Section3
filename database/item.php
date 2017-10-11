894fb4a342a1708a171907dfd971e7d17246746a
ammarhus1 <ammarhus@live.com>
2017-10-10 20:48:38 -0400

    Updated database to use item functions

--- database/item.php
+++ database/item.php
@@ -10,10 +10,10 @@
 
 $inventory = array();
 
-$inventory[] = new Item(0,"Gold","System wide currency.","money",1,1,1);
-$inventory[] = new Item(1,"Medicine","Sophisticated medicine capable of healing half of the body’s wounds.","health",0,50,25);
-$inventory[] = new Item(2,"Elixir","Very advanced medicine capable of healing all of the body’s wounds.","health",0,100,50);
-$inventory[] = new Item(3,"Mineral","Resource found mainly on mining colonies. Used to make materials.","raw",0,500,250);
-$inventory[] = new Item(4,"Rare Mineral","Rare resource found mainly on mining colonies. Used to make higher end materials.","raw",0,1000,500);
+$inventory[] = Item::inventoryItem(0,"Gold","System wide currency.","money",1,1);
+$inventory[] = Item::inventoryItem(1,"Medicine","Sophisticated medicine capable of healing half of the body’s wounds.","health",50,25);
+$inventory[] = Item::inventoryItem(2,"Elixir","Very advanced medicine capable of healing all of the body’s wounds.","health",100,50);
+$inventory[] = Item::inventoryItem(3,"Mineral","Resource found mainly on mining colonies. Used to make materials.","raw",500,250);
+$inventory[] = Item::inventoryItem(4,"Rare Mineral","Rare resource found mainly on mining colonies. Used to make higher end materials.","raw",1000,500);
 
 ?>
