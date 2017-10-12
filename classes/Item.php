19634d4c968de1a5047526bd6c338ee58b898fb9
jnbmaster <jnb.cb38@gmail.com>
2017-10-11 20:16:24 -0400

    Edited weapon class and added weapons to item database

--- classes/Item.php
+++ classes/Item.php
@@ -52,9 +52,56 @@ class Item{
     }
 
 }
+
+/**
+    Weapon class
+    Used in: database/item.php
+    Accesses: Item class - classes/item.php
+ */
 class Weapon extends Item {
-    public $power;
-    public $level;
-    public $upgradeValue;
+    private $power; // int damage of weapon
+    private $level; // int upgrade level. starts at 0, +1 for each upgrade
+    private $upgradeValue; // int amount that weapon increases by for every upgrade
+    private $upgradeCost; // int cost of minerals that it takes to upgrade weapon. increases by 1 for every upgrade 
+
+    public static function createWeapon( $id, $name, $description, $type, $buyValue, $sellValue, $power, $upgradeValue) {
+        $instance = new self();
+        $instance->id=$id;
+        $instance->name=$name;
+        $instance->description=$description;
+        $instance->type=$type;
+        $instance->amount=0;
+        $instance->buyValue=$buyValue;
+        $instance->sellValue=$sellValue;
+        $instance->power = $power;
+        $instance->upgradeValue = $upgradeValue;
+        $instance->level=0;
+        $instance->upgradeCost = 1;
+        return $instance;
+    }
+
+    public function upgradeWeapon(){
+        $this->power += $upgradeValue;
+        $this->level += 1;
+        $this->upgradeCost += 1;
+        // need logic somewhere for reduce minerals in player inventory
+    }
+
+    public function getPower(){
+        return $this->power;
+    }
+
+    public function getLevel(){
+        return $this->level;
+    }
+
+    public function getUpgradeValue(){
+        return $this->upgradeValue;
+    }
+
+    public function getUpgradeCost(){
+        return $this->upgradeCost;
+    }
+
 }
 ?>
--- database/item.php
+++ database/item.php
@@ -1,4 +1,11 @@
 <?php
+
+/**
+	Holds information for items
+	Used in: 
+	Accesses: classes/item.php
+*/
+
 #    _____ _                       _       _        _
 #   |_   _| |                     | |     | |      | |
 #     | | | |_ ___ _ __ ___     __| | __ _| |_ __ _| |__   __ _ ___  ___
@@ -15,5 +22,14 @@ $inventory[] = Item::inventoryItem(1,"Medicine","Sophisticated medicine capable 
 $inventory[] = Item::inventoryItem(2,"Elixir","Very advanced medicine capable of healing all of the body’s wounds.","health",100,50);
 $inventory[] = Item::inventoryItem(3,"Mineral","Resource found mainly on mining colonies. Used to make materials.","raw",500,250);
 $inventory[] = Item::inventoryItem(4,"Rare Mineral","Rare resource found mainly on mining colonies. Used to make higher end materials.","raw",1000,500);
+
+$weaponList = array();
+
+//$weaponList[] = Weapon::createWeapon($id, $name, $description, $type, $buyValue, $sellValue, $power, $upgradeValue);
+$weaponList[] = Weapon::createWeapon(0, "Plasma Knife", "Very common self-defense weap-on popular among colonists.", "weapon", 50, 25, 1, 1);
+$weaponList[] = Weapon::createWeapon(1, "Plasma Sword", "Old weapon mainly found in museums around the colony, no longer common place but more powerful than the knife.", "weapon", 125, $65, 3, 2);
+$weaponList[] = Weapon::createWeapon(2, "Plasma Pistol", "Inexpensive gun used by colony law enforcement to protect from space pirates. Easy to find in shops.", "weapon", 250, 125, 5, 3);
+$weaponList[] = Weapon::createWeapon(3, "Laser Rifle", "Military grade weaponry used by colony militia’s. Expensive.", "weapon", 600, 300, 7, 5);
+$weaponList[] = Weapon::createWeapon(4, "Rocket Launcher", "Very powerful weapon only used by highly trained soldiers. The limited availability of this weapon makes it expensive in shops.", "weapon", 1000, 750, 9, 8);
 
 ?>
