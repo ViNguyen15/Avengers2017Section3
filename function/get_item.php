579f9b8a5020a092b9dd81a725e19f692be4e06e
ammarhus1 <ammarhus@live.com>
2017-10-11 20:35:13 -0400

    Fixed bug not deleting item

--- function/get_item.php
+++ function/get_item.php
@@ -9,7 +9,7 @@ session_start();
 
 $value = $_GET['item'];
 $values = explode(",", $value);
-$location = explode(":",$_SESSION['location']);
+$location = $_SESSION['location'];
 
 
 
@@ -21,7 +21,7 @@ foreach ($_SESSION['inventory'] as $item){
 
 foreach ($_SESSION['game'] as $planet) {
     foreach ($planet->rooms as $room){
-        if ($room->id==$location[1]) {
+        if ($room->id==$location->id) {
             foreach ($room->items as $i => $item){
                 if ($item->dropid == $values[2]){
                     unset($room->items[$i]);
