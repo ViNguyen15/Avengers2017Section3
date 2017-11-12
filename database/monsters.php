86d38c12bd90b3552a0ced2630e6c474bb3bc1d3
jnbmaster <jnb.cb38@gmail.com>
2017-11-12 17:36:32 -0500

    Made changes to monster

--- database/monsters.php
+++ database/monsters.php
@@ -1,16 +1,14 @@
 <?php
-//include "../classes/Monster.php";
-// need to assign items to monsters
 $monsterList = array();
-$monsterList[] = new Monster(0, "Space Pirate", "Infamous space pirates known for causing havoc system wide. They are despised by all colonies. They are armored with light weaponry and armor.", 6, 15, NULL, 100);
-$monsterList[] = new Monster(1, "Space-Pirate Captain" ,"Commander of all space pirates in the solar System. He is heavily armored and has powerful weaponry.", 10, 55, NULL, 175);
-$monsterList[] = new Monster(2, "Defense Drone", "These defense drones were created to defend research stations from space pirates, however an evil scientist has hacked them and turned them against the player! They attack with a deadly laser but are somewhat weak.", 7, 15, NULL, 25);
-$monsterList[] = new Monster(3, "Mad Scientist" , "This evil scientist has been hired by the pace pirates to steal technology from research stations. He has hacked defense drones to do his bidding", 8, 45, NULL, 200);
-$monsterList[] = new Monster(4, "Kraken", "Huge green sea creature that resides in an ocean moon of Neptune. Frequently terrorizes colonists with its spiky tentacles", 12, 120, NULL, 300);
-$monsterList[] = new Monster(5, "Fire Giants" ,"Fire resistant monsters native to volcanic moons.", 9, 55, NULL, 150);
-$monsterList[] = new Monster(6, "Worn-out Robot", "Old robot that you find on the surface of earth. It’s not very strong.", 5, 10, NULL, 10);
-$monsterList[] = new Monster(7, "Ruffians" ,"Thieves that disrupt and steal from colonists", 4, 12, NULL, 50);
-//$monsterList[] = new Monster(id, name ,description, strength, healthpoints, NULL, goldamount);
+$monsterList[] = new Monster(0, "Space Pirate", "Infamous space pirates known for causing havoc system wide. They are despised by all colonies. They are armored with light weaponry and armor.", 6, 15);
+$monsterList[] = new Monster(1, "Space-Pirate Captain" ,"Commander of all space pirates in the solar System. He is heavily armored and has powerful weaponry.", 10, 55);
+$monsterList[] = new Monster(2, "Defense Drone", "These defense drones were created to defend research stations from space pirates, however an evil scientist has hacked them and turned them against the player! They attack with a deadly laser but are somewhat weak.", 7, 15);
+$monsterList[] = new Monster(3, "Mad Scientist" , "This evil scientist has been hired by the pace pirates to steal technology from research stations. He has hacked defense drones to do his bidding", 8, 45);
+$monsterList[] = new Monster(4, "Kraken", "Huge green sea creature that resides in an ocean moon of Neptune. Frequently terrorizes colonists with its spiky tentacles", 12, 120);
+$monsterList[] = new Monster(5, "Fire Giants" ,"Fire resistant monsters native to volcanic moons.", 9, 55);
+$monsterList[] = new Monster(6, "Worn-out Robot", "Old robot that you find on the surface of earth. It’s not very strong.", 5, 10);
+$monsterList[] = new Monster(7, "Ruffians" ,"Thieves that disrupt and steal from colonists", 4, 12);
+//$monsterList[] = new Monster(id, name ,description, strength, healthpoints);
 
 
  ?>
