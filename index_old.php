8c3e8e583f4425d87d1a2a89d81c2346eb85dfa2
ammarhus1 <ammarhus@live.com>
2017-11-16 00:57:47 -0500

    Old stuff incase new stuff breaks

---
+++ index_old.php
@@ -0,0 +1,75 @@
+<?php 
+foreach (glob("interface/*.php") as $interface) {
+    include("$interface");
+}
+foreach (glob("classes/*.php") as $class) {
+    include("$class");
+}
+
+//Include all of the classes
+
+session_start();
+//Starts the session
+
+
+if ($_SESSION['loggedin']!=true){
+    header('Location: register.php');
+}
+//if user is not logged in, redirect to Register.php
+
+?>
+<script src="runtime.js?v=<?php echo time();?>" type="text/javascript"></script>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<head>
+    <title>Text-Based Game</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" type="text/css" href="styles.css?v=<?php echo time();?>" />
+    
+</head>
+
+<body>
+<header>
+    <h1>Project Voyager</h1>
+
+    <form action="function/logout.php" method="post"><input type="submit" value="Logout"></form>
+    <form action="function/save.php" method="post"><input type="submit" value="Save Game"></form>
+
+</header>
+
+<center>
+<nav>
+
+    <img src="images/directions.png" alt="" usemap="#Map" />
+    <br>
+    <?php 
+    echo "Username: ".$_SESSION['player']->name;
+    ?>
+
+    <br>
+    <player id="playerinfo">
+
+    </player>
+
+    
+    <map name="Map" id="Map">
+        <area alt="" title="" onclick="go(1)" href="#" shape="poly" coords="101,85,135,56,134,2,68,1,66,56" />
+        <area alt="" title="" onclick="go(2)" href="#" shape="poly" coords="117,99,146,134,196,133,198,67,144,67" />
+        <area alt="" title="" onclick="go(3)" href="#" shape="poly" coords="99,113,134,142,134,196,69,198,67,144" />
+        <area alt="" title="" onclick="go(4)" href="#" shape="poly" coords="85,99,56,66,1,67,0,133,56,133" />
+    </map>
+</nav>
+
+<game>
+    <div id="room">
+
+    </div>
+    <user id="draw" style="<?php echo 'left:'.($_SESSION['player']->x * 32 ).'; top:'.($_SESSION['player']->y * 32 )?>">
+        <img src='images/player.png' height=64px width=32px />
+    </user>
+
+    <button onclick="Controller('takeDamage',10)">Take 10 Damage</button>
+</game>
+
+</center>
+</body>
