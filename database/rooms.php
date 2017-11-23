88f1341dd231c81d3563143e219c579bf4fa5394
zehra-fs <sarahandzehra@gmail.com>
2017-11-23 12:01:58 -0500

    Room tiles

--- database/rooms.php
+++ database/rooms.php
@@ -535,15 +535,15 @@ $rooms[38]->addEntities( array (
 
 $rooms[] = new Shop(50, "Shop", "This is the home world shop.");
 
-$rooms[] = new Puzzle(60, "What are Saturn’s Rings made of?", "Ice", "Dust, Ice, or Boron", 7, 1);
+$rooms[] = new Puzzle(60, "What are Saturn’s rings made of?", "Ice", "Dust, Ice, or Boron", 7, 1);
 $rooms[] = new Puzzle(61, "True or False: Saturn is less dense than water.", "True", "Saturn can float on water", 0, 150);
-$rooms[] = new Puzzle(62, "Saturn was Discovered by?", "Galileo", "Isaac Newton, Copernicus, or Galileo", 2, 2);
+$rooms[] = new Puzzle(62, "Saturn was Discovered by ______.", "Galileo", "Isaac Newton, Copernicus, or Galileo", 2, 2);
 $rooms[] = new Puzzle(63, "What kind of natural disaster happens on the moon?", "Moonquake", "Not an earthquake", 0, 200);
 $rooms[] = new Puzzle(64, "Sometimes I’m full, but I never overflow. What am I?", "The moon", "Not the sun", 0, 150);
-$rooms[] = new Puzzle(65, "It dances bright, Banishing all but darkest night. Give it food and it will live; Give it water and it will die. What is it?", "Fire", "Everything changed when the __ nation attacked", 5, 1);
-$rooms[] = new Puzzle(66, "Sometimes you can see my tail. I’m not a planet. I am _.", "Comet", "I visit earth every 76 years", 18, 1);
+$rooms[] = new Puzzle(65, "It dances bright, banishing all but darkest night. Give it food and it will live; Give it water and it will die. What is it?", "Fire", "Everything changed when the __ nation attacked", 5, 1);
+$rooms[] = new Puzzle(66, "Sometimes you can see my tail. I’m not a planet. I am _____.", "Comet", "I visit earth every 76 years", 18, 1);
 $rooms[] = new Puzzle(67, "How many years does it take for a solar eclipse to occur?", "1.5", "Eighteen months", 0, 300);
-$rooms[] = new Puzzle(68, "How many days does it take mercenary to orbit the sun?", "88", "I ate and ate", 0, 200);
+$rooms[] = new Puzzle(68, "How many days does it take Mercenary to orbit the sun?", "88", "I ate and ate", 0, 200);
 $rooms[] = new Puzzle(69, "True or False: The Grand Canyon is the biggest canyon in the solar system.", "False", "There is a grander canyon", 0, 150);
 
 $default_room = $rooms[9];
